44071ed0c3086e3d8b77f475a047497f59127e11
Tanmay Joshi <tanmayjoshi579@gmail.com>
2026-05-10 14:42:13 +0530

    Routes created

---
+++ src/routes/schoolRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const {
+    addSchool,
+    listSchools
+} = require("../controllers/schoolController");
+
+const router = express.Router();
+
+router.post("/addSchool", addSchool);
+
+router.get("/listSchools", listSchools);
+
+module.exports = router;
